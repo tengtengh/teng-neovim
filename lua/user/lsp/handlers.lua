222d091e35d699ac676ac717489e8cda8142ec94
tengtengh <10678859+tengtengh@user.noreply.gitee.com>
2022-10-18 14:28:30 +0800

    change lsp DiagnosticSign icon

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -2,12 +2,12 @@ local M = {}
 
 
 M.setup = function()
-    -- local signs = {
-    --   { name = "DiagnosticSignError", text = "" },
-    --   { name = "DiagnosticSignWarn", text = "" },
-    --   { name = "DiagnosticSignHint", text = "" },
-    --   { name = "DiagnosticSignInfo", text = "" },
-    -- }
+    local signs = {
+      { name = "DiagnosticSignError", text = "" },
+      { name = "DiagnosticSignWarn", text = "" },
+      { name = "DiagnosticSignHint", text = "" },
+      { name = "DiagnosticSignInfo", text = "" },
+    }
     --
     -- for _, sign in ipairs(signs) do
     --   vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
@@ -16,6 +16,7 @@ M.setup = function()
     local config = {
         -- disable virtual text
         virtual_text = false,
+        -- virtual_text = true,
 
         -- show signs
         signs = {
