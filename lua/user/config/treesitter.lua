351029824bfcf1fe11e65fa3af3e808a1bcb2d23
tengtengh <63461078+tengtengh@users.noreply.github.com>
2023-03-13 17:32:32 +0800

    treesitter

--- lua/user/config/treesitter.lua
+++ lua/user/config/treesitter.lua
@@ -32,9 +32,9 @@ nvim_treesitter_config.setup({
     -- -- List of parsers to ignore installing (for "all")
     -- ignore_install = { "javascript" },
 
-    -- highlight = {
-    --     -- `false` will disable the whole extension
-    --     enable = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
 
     --     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
     --     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
@@ -47,5 +47,5 @@ nvim_treesitter_config.setup({
     --     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     --     -- Instead of true it can also be a list of languages
     --     additional_vim_regex_highlighting = false,
-    -- }
+    }
 })
